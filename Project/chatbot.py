310e585d5c03ea69876a7b0910cae264b2f2547d
Trung Đàm Thành <trung1qaz2wsx3edc4rfv@gmail.com>
2024-01-20 13:18:31 -0500

    a

---
+++ Project/chatbot.py
@@ -0,0 +1,41 @@
+import random
+import pickle
+import numpy as np
+
+inport nltk
+from nltk.stem inport WordNetLemmatizer
+
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.model')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words)
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key = lambda x: x[i], reverse = True)
+    return_list = []
+    for r in results:
+        return_list.append({'intents': classes[r[0], 'probability': str(r[1])]})
+    return return_list
